1114fc26ddae693e73fc603f57147eb94fb55031
BSSE24040 <bsse24040@itu.edu.pk>
2026-07-04 16:20:21 +0500

    Time: 5 ms (19.86%), Space: 48.7 MB (99.62%) - LeetHub

---
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int count=0;
+        
+        for(int i=0;i<fruits.size();i++){
+            bool flag=false;
+            for(int j=0;j<baskets.size();j++){
+
+            if(fruits[i]<=baskets[j]){
+                flag=true;
+                baskets[j]=-1;
+                break;
+            }
+            }
+            if(!flag){
+                count++;
+            }
+        }
+        return count;
+    }
+};
