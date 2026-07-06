1bba60e862118c9272d4c08333a1d901954da15b
BSSE24040 <bsse24040@itu.edu.pk>
2026-07-06 18:28:14 +0500

    Time: 1 ms (35.84%), Space: 97.3 MB (87.52%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+ int maxProfit(vector<int>& prices) {
+
+        int mn=prices[0];
+        int profit=0;
+
+        for (int i=1;i<prices.size();i++) {
+
+            if (prices[i]<mn) {
+                mn=prices[i];
+            }
+
+            profit=max(profit,prices[i]-mn);
+        }
+
+        return profit;
+    }
+};
