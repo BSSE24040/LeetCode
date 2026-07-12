b94425a167e89e710ad9914f7fa40265821a37e9
BSSE24040 <bsse24040@itu.edu.pk>
2026-07-12 15:38:46 +0500

    Time: 2 ms (2.94%), Space: 11.8 MB (11.66%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+int count=nums.size();
+        for(int i=0;i<count-1;i++){
+            for(int j=0;j<count-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+        
+    }
+};
