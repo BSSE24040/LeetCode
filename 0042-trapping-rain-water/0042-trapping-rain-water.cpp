2f0df45e6f845e89c23c3f8853392aec835a2e8d
BSSE24040 <bsse24040@itu.edu.pk>
2026-07-22 18:28:18 +0500

    Time: 0 ms (100%), Space: 26 MB (57.73%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int l=0;
+        int r=height.size()-1;
+int water=0;
+int leftMax=0;
+int rightMax=0;
+        while(l<r){
+          
+
+          if(height[l]<height[r]){
+            if(height[l]>=leftMax){
+                leftMax=height[l];
+            }
+            else{
+                water+=leftMax-height[l];
+            }
+l++;
+          }
+          else{
+           
+                if(height[r]>=rightMax){
+                    rightMax=height[r];
+                }
+                else{
+                    water+=rightMax-height[r];
+                }
+                r--;
+            
+          }
+         
+        }
+ return water;
+
+        
+    }
+};
