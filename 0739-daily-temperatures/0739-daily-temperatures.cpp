dc845f0439e8ab8b82b316197dfda8796653298d
BSSE24040 <bsse24040@itu.edu.pk>
2026-07-22 16:14:44 +0500

    Time: 20 ms (63.67%), Space: 107.5 MB (18.36%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+                 int n=temperatures.size();
+           vector<int> ans(n,0);
+
+
+           stack<int>st;
+           int count=0;
+
+           for(int i=0;i<temperatures.size();i++){
+
+
+      while(!st.empty()&&temperatures[i]>temperatures[st.top()]){
+
+
+        int index=st.top();
+        st.pop();
+        ans[index]=i-index;
+      }
+      st.push(i);
+            
+           }
+return ans;
+
+           
+
+
+    }
+};
