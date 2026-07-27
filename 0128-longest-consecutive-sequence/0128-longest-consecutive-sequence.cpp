204644747df831f2792f82efafa09c1af69f0b0e
BSSE24040 <bsse24040@itu.edu.pk>
2026-07-27 17:03:22 +0500

    Time: 85 ms (37.15%), Space: 88.8 MB (67.37%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -4,22 +4,22 @@ public:
 
         unordered_set<int> st(nums.begin(), nums.end());
 
-        int longest = 0;
+        int longest=0;
 
-        for(int num : st) {
+        for(int num:st) {
 
            
-            if(st.find(num - 1) == st.end()) {
+            if(st.find(num-1)==st.end()) {
 
-                int current = num;
-                int count = 1;
+                int current=num;
+                int count=1;
 
-                while(st.find(current + 1) != st.end()) {
+                while(st.find(current+1)!=st.end()) {
                     current++;
                     count++;
                 }
 
-                longest = max(longest, count);
+                longest=max(longest,count);
             }
         }
 
