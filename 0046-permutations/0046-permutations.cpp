459b7075295eee6656e7c036a462beda94c5aa06
BSSE24040 <bsse24040@itu.edu.pk>
2026-08-22 16:44:55 +0500

    Time: 4 ms (9.88%), Space: 10.9 MB (27.55%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+   vector<vector<int>> result;
+   void backtrack(vector<int>&current,vector<int>&nums,vector<bool>&used){
+
+
+    if(current.size()==nums.size()){
+        result.push_back(current);
+        return;
+    }
+   
+
+   for(int i=0;i<nums.size();i++){
+
+
+if(used[i]){
+    continue;
+}
+
+
+used[i]=true;
+    current.push_back(nums[i]);
+
+    backtrack(current,nums,used);
+
+    current.pop_back();
+    used[i]=false;
+   }
+
+   }
+   
+    vector<vector<int>> permute(vector<int>& nums) {
+vector<bool>used(nums.size(),false);
+vector<int>current;
+   backtrack(current,nums,used);
+        return result;
+    }
+};
